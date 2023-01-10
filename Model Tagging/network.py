9d02b0ecf803286a80ddf7aaad1cc79cf79a17ca
mlenover <mlenover@gmail.com>
2023-01-09 19:03:05 -0500

    Implemented basic neural network structure

--- Model Tagging/network.py
+++ Model Tagging/network.py
@@ -5,12 +5,31 @@ from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
 import torch.optim as optim
 import torchdata.datapipes as dp
+import csv
+import numpy as np
 
 device = "cuda" if torch.cuda.is_available else "cpu"
 print(f"Using {device} device")
 
+data = []
 
 FOLDER = '/data/'
+with open(os.getcwd() + FOLDER + 'Data File.csv', 'r', newline='') as file:
+    reader = csv.reader(file)
+    for row in reader:
+        data.append(row)
+
+data = np.array(data).astype(float)
+
+labels = data[:,0].flatten()
+labels = torch.from_numpy(labels)
+labels = labels.to(device=device, dtype=torch.long)
+
+train_data = data[:,1:]
+train_data = np.rot90(train_data)
+train_data = torch.from_numpy(data[:,1:])
+train_data = train_data.to(device=device, dtype=torch.float32)
+
 datapipe = dp.iter.FileLister([FOLDER]).filter(filter_fn=lambda filename: filename.endswith('.csv'))
 datapipe = dp.iter.FileOpener(datapipe, mode='rt')
 datapipe = datapipe.parse_csv(delimiter=',')
@@ -20,7 +39,6 @@ train, valid = datapipe.random_split(total_length=N_ROWS, weights={"train": 0.5,
 class NeuralNetwork(nn.Module):
     def __init__(self):
         super(NeuralNetwork, self).__init__()
-        self.flatten = nn.Flatten()
         self.linear_relu_stack = nn.Sequential(
             nn.Linear(61, 35),
             nn.ReLU(),
@@ -36,8 +54,8 @@ class NeuralNetwork(nn.Module):
         )
         
     def forward(self, x):
-        x = self.flatten(x)
         logits = self.linear_relu_stack(x)
+        #logits = nn.functional.Softmax(logits, dim=1)
         return logits
 
 model = NeuralNetwork().to(device)
@@ -46,8 +64,15 @@ print(model)
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters())
 
-for epoch in range(2):
+for epoch in range(100):
     
     running_loss = 0.0
-    for i, data in enumerate(trainloader, 0):
-        inputs, labels = data
+    for i, sample in enumerate(train_data):
+        outputs = model(sample)
+        loss = criterion(outputs, labels[i])
+        loss.backward()
+        optimizer.step()
+        
+        if (i+1) % 100 == 0:
+            print ('Epoch [%d/%d], Iter [%d] Loss: %.4f' %(epoch+1, 10, i+1, loss.item()))
+        
